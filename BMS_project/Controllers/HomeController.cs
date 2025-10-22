b1a5b8f245de02c9571927c504123801a9700937
AFaeldo <faeldoaiken@gmail.com>
2025-10-22 15:59:14 +0800

    update

--- BMS_project/Controllers/HomeController.cs
+++ BMS_project/Controllers/HomeController.cs
@@ -6,28 +6,15 @@ namespace BMS_project.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            ViewData["Title"] = "Home Page";
+            ViewData["Title"] = "Home";
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //public IActionResult About()
+        //{
+        //    ViewData["Title"] = "About";   
+        // return View();
+        //}
     }
 }
