5744f0a810393083fd1a872baecc271ad1201099
AFaeldo <faeldoaiken@gmail.com>
2025-10-22 21:22:49 +0800

    oki na

--- BMS_project/Program.cs
+++ BMS_project/Program.cs
@@ -25,7 +25,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting(); //\load for images/css/js
+app.UseRouting(); //load for images/css/js
 
 //Enable Authentication & Authorization Middleware
 app.UseAuthentication();
