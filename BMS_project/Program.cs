f3562e32ef4f9d67bbe367f9d19219b24a0ec17b
Burrento <bolanos.brentpaulo07@gmail.com>
2025-10-26 21:23:54 +0800

    fixed

--- BMS_project/Program.cs
+++ BMS_project/Program.cs
@@ -44,9 +44,10 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    //pattern: "{controller=Home}/{action=Index}/{id?}")
     //pattern: "{controller=SuperAdmin}/{action=Dashboard}/{id?}")
     //pattern: "{controller=BarangaySK}/{action=Dashboard}/{id?}")
+    pattern: "{controller=FederationPresident}/{action=Dashboard}/{id?}")
     .WithStaticAssets();
 
 app.Run();
