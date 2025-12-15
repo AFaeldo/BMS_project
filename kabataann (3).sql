a69a73b820e3262d410925f50eaf9979f7cfe952
Brent Paulo Bolanos <bolanos.brentpaulo07@gmail.com>
2025-12-15 09:19:31 +0800

    Add files via upload

---
+++ kabataann (3).sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:8080
+-- Generation Time: Dec 15, 2025 at 02:17 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kabataann`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcement`
+--
+-- Error reading structure for table kabataann.announcement: #1932 - Table &#039;kabataann.announcement&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.announcement: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`announcement`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barangay`
+--
+-- Error reading structure for table kabataann.barangay: #1932 - Table &#039;kabataann.barangay&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.barangay: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`barangay`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budget`
+--
+-- Error reading structure for table kabataann.budget: #1932 - Table &#039;kabataann.budget&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.budget: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`budget`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `compliance`
+--
+-- Error reading structure for table kabataann.compliance: #1932 - Table &#039;kabataann.compliance&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.compliance: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`compliance`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `compliance_document`
+--
+-- Error reading structure for table kabataann.compliance_document: #1932 - Table &#039;kabataann.compliance_document&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.compliance_document: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`compliance_document`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `federation_fund`
+--
+-- Error reading structure for table kabataann.federation_fund: #1932 - Table &#039;kabataann.federation_fund&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.federation_fund: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`federation_fund`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_upload`
+--
+-- Error reading structure for table kabataann.file_upload: #1932 - Table &#039;kabataann.file_upload&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.file_upload: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`file_upload`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kabataan_service_record`
+--
+-- Error reading structure for table kabataann.kabataan_service_record: #1932 - Table &#039;kabataann.kabataan_service_record&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.kabataan_service_record: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`kabataan_service_record`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kabataan_term_period`
+--
+-- Error reading structure for table kabataann.kabataan_term_period: #1932 - Table &#039;kabataann.kabataan_term_period&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.kabataan_term_period: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`kabataan_term_period`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+-- Error reading structure for table kabataann.login: #1932 - Table &#039;kabataann.login&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.login: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`login`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+-- Error reading structure for table kabataann.project: #1932 - Table &#039;kabataann.project&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.project: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`project`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_allocation`
+--
+-- Error reading structure for table kabataann.project_allocation: #1932 - Table &#039;kabataann.project_allocation&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.project_allocation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`project_allocation`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_document`
+--
+-- Error reading structure for table kabataann.project_document: #1932 - Table &#039;kabataann.project_document&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.project_document: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`project_document`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_log`
+--
+-- Error reading structure for table kabataann.project_log: #1932 - Table &#039;kabataann.project_log&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.project_log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`project_log`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+-- Error reading structure for table kabataann.role: #1932 - Table &#039;kabataann.role&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.role: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`role`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sitio`
+--
+-- Error reading structure for table kabataann.sitio: #1932 - Table &#039;kabataann.sitio&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.sitio: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`sitio`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_log`
+--
+-- Error reading structure for table kabataann.system_log: #1932 - Table &#039;kabataann.system_log&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.system_log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`system_log`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table kabataann.user: #1932 - Table &#039;kabataann.user&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`user`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `youth_member`
+--
+-- Error reading structure for table kabataann.youth_member: #1932 - Table &#039;kabataann.youth_member&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.youth_member: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`youth_member`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `__efmigrationshistory`
+--
+-- Error reading structure for table kabataann.__efmigrationshistory: #1932 - Table &#039;kabataann.__efmigrationshistory&#039; doesn&#039;t exist in engine
+-- Error reading data for table kabataann.__efmigrationshistory: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kabataann`.`__efmigrationshistory`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
